84704c42a07760efa4b02d1f1d1e4aafe6a38c56
Yangyan Li <yangyan@stanford.edu>
2016-04-12 23:05:49 +0800

    Implement normal field layer

--- src/caffe/layers/normal_field_layer.cpp
+++ src/caffe/layers/normal_field_layer.cpp
@@ -21,6 +21,53 @@ void NormalFieldLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom, const 
 
 template<typename Dtype>
 void NormalFieldLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  const std::vector<int>& field_shape = bottom[0]->shape();
+  int batch_size = field_shape[0];
+  int grid_dim = field_shape[1];
+  int grid_dim_1 = grid_dim-1;
+
+  std::vector<int> offset(5, 0);
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  Dtype* top_data = top[0]->mutable_cpu_data();
+  for (int i = 0; i < grid_dim; ++ i) {
+    offset[1] = i;
+    Dtype x = i + 0.5;
+    int x0, x1;
+    Dtype x_a, x_m;
+    SnapGrid_cpu(x, x0, x1, x_a, x_m, grid_dim_1);
+    Dtype x_x0 = x-x0;
+    Dtype x1_x = x1-x;
+    for (int j = 0; j < grid_dim; ++ j) {
+      offset[2] = j;
+      Dtype y = j + 0.5;
+      int y0, y1;
+      Dtype y_a, y_m;
+      SnapGrid_cpu(y, y0, y1, y_a, y_m, grid_dim_1);
+      Dtype y_y0 = y-y0;
+      Dtype y1_y = y1-y;
+      for (int k = 0; k < grid_dim; ++ k) {
+        offset[3] = k;
+        Dtype z = k + 0.5;
+        int z0, z1;
+        Dtype z_a, z_m;
+        SnapGrid_cpu(z, z0, z1, z_a, z_m, grid_dim_1);
+        Dtype z_z0 = z-z0;
+        Dtype z1_z = z1-z;
+        for (int batch_idx = 0; batch_idx < batch_size; ++ batch_idx) { 
+          offset[0] = batch_idx;
+          Dtype nx, ny, nz;
+          ComputeGradient_cpu(bottom_data, batch_idx, x0, y0, z0, x1, y1, z1,
+            x_a, y_a, z_a, x_m, y_m, z_m, x_x0, y_y0, z_z0, x1_x, y1_y, z1_z,
+            nx, ny, nz, grid_dim, grid_dim, grid_dim);
+          Normalize_cpu(nx, ny, nz);
+          int p = top[0]->offset(offset); 
+          top_data[p+0] = nx;
+          top_data[p+1] = ny; 
+          top_data[p+2] = nz; 
+        } /* batch_size */
+      } /* z */
+    } /* y */
+  } /* x */
 }
 
 #ifdef CPU_ONLY
--- src/caffe/layers/normal_field_layer.cu
+++ src/caffe/layers/normal_field_layer.cu
@@ -4,8 +4,54 @@
 
 namespace caffe {
 
+template <typename Dtype>
+__global__ void NormalFieldForward(const int num_grids, const int grid_dim, const int batch_size,
+  const Dtype* bottom_data, Dtype* top_data) {
+  const int grid_idx = blockDim.x*blockIdx.x + threadIdx.x;
+  // One thread for each grid
+  if(grid_idx < num_grids) {
+    const int grid_dim_1 = grid_dim-1;
+    const int yz = grid_dim*grid_dim;
+    Dtype z = grid_idx%grid_dim + 0.5;
+    Dtype y = (grid_idx/grid_dim)%grid_dim + 0.5;
+    Dtype x = grid_idx/yz + 0.5;
+    int x0, y0, z0, x1, y1, z1;
+    Dtype x_a, y_a, z_a, x_m, y_m, z_m;
+    SnapGrid_gpu(x, x0, x1, x_a, x_m, grid_dim_1);
+    SnapGrid_gpu(y, y0, y1, y_a, y_m, grid_dim_1);
+    SnapGrid_gpu(z, z0, z1, z_a, z_m, grid_dim_1);
+    Dtype x_x0 = x-x0;
+    Dtype y_y0 = y-y0;
+    Dtype z_z0 = z-z0;
+    Dtype x1_x = x1-x;
+    Dtype y1_y = y1-y;
+    Dtype z1_z = z1-z;
+    Dtype nx, ny, nz;
+    for (int batch_idx = 0; batch_idx < batch_size; ++ batch_idx) {
+      ComputeGradient_gpu(bottom_data, batch_idx, x0, y0, z0, x1, y1, z1,
+        x_a, y_a, z_a, x_m, y_m, z_m, x_x0, y_y0, z_z0, x1_x, y1_y, z1_z,
+        nx, ny, nz, grid_dim, grid_dim, grid_dim);
+      Normalize_gpu(nx, ny, nz);
+      int p = (batch_idx*num_grids+grid_idx)*3;
+      top_data[p+0] = nx;
+      top_data[p+1] = ny;
+      top_data[p+2] = nz;
+    }
+  }
+}
+
 template<typename Dtype>
 void NormalFieldLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  const Dtype* bottom_data = bottom[0]->gpu_data();
+  const vector<int>& bottom_shape = bottom[0]->shape();
+  Dtype* top_data = top[0]->mutable_gpu_data();
+  const int batch_size = bottom_shape[0];
+  const int grid_dim = bottom_shape[1];
+  const int num_grids = bottom[0]->count(1);
+
+  // NOLINT_NEXT_LINE(whitespace/operators)
+  NormalFieldForward<Dtype><<<CAFFE_GET_BLOCKS(num_grids), CAFFE_CUDA_NUM_THREADS>>>(num_grids, grid_dim, batch_size, bottom_data, top_data);
+  CUDA_POST_KERNEL_CHECK;
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(NormalFieldLayer);
